e6fcd7f40848a46697bf04efb32cfc30a73ddc6a
ivanleonov <treska1315@gmail.com>
2021-11-22 18:38:44 +0300

    index added 3rd step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format', 'stylish')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    genDiff(filepath1, filepath2);
+  });
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+const readFile = (filename) => fs.readFileSync(path.resolve(process.cwd(), filename.trim()), 'utf-8');
+
+const compare = (obj1, obj2) => {
+  const res = {};
+  const arrkeys = Object.keys({ ...obj1, ...obj2 });
+  const keys = _.sortBy(arrkeys);
+  keys.forEach((key) => {
+    if (_.has(obj2, key) && _.has(obj1, key)) {
+      if (obj2[key] === obj1[key]) {
+        res[`  ${key}`] = obj2[key];
+      } else {
+        res[`- ${key}`] = obj1[key];
+        res[`+ ${key}`] = obj2[key];
+      }
+    } else if (!_.has(obj2, key) && _.has(obj1, key)) {
+      res[`- ${key}`] = obj1[key];
+    } else {
+      res[`+ ${key}`] = obj2[key];
+    }
+  });
+  return res;
+};
+
+const genDiff = (ftree, stree) => {
+  const firstTree = JSON.parse(readFile(ftree));
+  const secondTree = JSON.parse(readFile(stree));
+  console.log(compare(firstTree, secondTree));
+};
+
+export default genDiff;
