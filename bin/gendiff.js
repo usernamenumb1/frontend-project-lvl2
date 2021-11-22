d839cbe94bae9545e8673b69aea989a1927ab1e6
ivanleonov <treska1315@gmail.com>
2021-11-22 15:10:24 +0300

    format & arguments added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,7 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>');
 program.parse();
