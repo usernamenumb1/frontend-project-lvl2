4f97ff1047195ac89abc7b78087339e04eb770e7
ivanleonov <treska1315@gmail.com>
2021-11-24 14:12:29 +0300

    eslint plugin jest installed 2nd attempt

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,11 +5,10 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-//const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('gendiff', () => {
-  // const file1 = getFixturePath('file1.json');
-  // const file2 = getFixturePath('file2.json');
-  const res = genDiff('file1.json', 'file2.json');
-  expect(res).toEqual('{"- follow":false,"  host":"hexlet.io","- proxy":"123.234.53.22","- timeout":50,"+ timeout":20,"+ verbose":true}');
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
+  expect(genDiff(file1, file2)).toEqual('{"- follow":false,"  host":"hexlet.io","- proxy":"123.234.53.22","- timeout":50,"+ timeout":20,"+ verbose":true}');
 });
